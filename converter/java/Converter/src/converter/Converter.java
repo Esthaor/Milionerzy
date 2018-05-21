7c3d3a130b77df9b2b0772e7a65461064c143a95
Grzegorz Majchrzak <gregiem94@gmail.com>
2018-05-21 02:53:06 +0200

    Converter

---
+++ converter/java/Converter/src/converter/Converter.java
@@ -0,0 +1,18 @@
+package converter;
+
+/**
+ * 
+ *
+ * @since 2018-05-21, 02:46:04
+ * @author Grzegorz Majchrzak
+ */
+public class Converter {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
